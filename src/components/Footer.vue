747f435c782eae8da18dc05f5868e658b7270352
Dima <pyts97@mail.ru>
2024-03-17 00:40:06 +0300

    added contact information

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,11 @@
 <template>
-   <footer class="p-4 text-sm md:text-base">
-     <h2 class="font-bold">Контакты</h2>
-     <p>Телеграм:</p>
-     <p>Номер телефона:</p>
-   </footer>
- </template>
- 
+  <footer class="p-4 text-sm md:text-base">
+    <h2 class="font-bold">Контакты:</h2>
+    <ul>
+      <li>
+        Телеграм: <a href="https://t.me/biskv" class="hover:text-blue-400" target="_blank">biskv</a>
+      </li>
+      <li>Номер телефона: +79936302580</li>
+    </ul>
+  </footer>
+</template>
